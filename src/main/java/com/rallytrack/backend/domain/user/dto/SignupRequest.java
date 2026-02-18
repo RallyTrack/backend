c4f18e4de2cadb53ac8f2c8b0fb2b929ca407ec7
junmin <coom1222@gmail.com>
2026-02-19 00:27:29 +0900

    회원가입시 비밀번호 길이 제한

--- src/main/java/com/rallytrack/backend/domain/user/dto/SignupRequest.java
+++ src/main/java/com/rallytrack/backend/domain/user/dto/SignupRequest.java
@@ -1,7 +1,9 @@
 package com.rallytrack.backend.domain.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,7 +14,8 @@ public class SignupRequest {
     @NotBlank
     private String email;
 
-    @NotBlank
+    @Schema(description = "비밀번호 (8~20자), 특수기호나 대소문자 제약은 없음", example = "mypass1234!")
+    @Size(min = 8, max = 20)
     private String password;
 
     @NotBlank
