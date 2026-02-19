7b444934b44051c3b14d6e246fe5a153bed06078
junmin <coom1222@gmail.com>
2026-02-19 23:44:44 +0900

    S3 bucket 환경설정

---
+++ src/main/java/com/rallytrack/backend/config/S3Config.java
@@ -0,0 +1,33 @@
+package com.rallytrack.backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    // @Value로 application.yml에서 값을 읽어옴
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region}")
+    private String region;
+
+    // AWS에서 만들어 놓은 클래스. S3 버킷에 파일 업로드, 다운로드, 삭제 등의 기능이 들어있음
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)))
+                .build();
+    }
+}
